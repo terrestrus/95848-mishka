573c21108d45a8d135c7e222f7afd07372bb22a1
Eugene <terrestrus@gmail.com>
2017-03-22 23:06:41 +0300

    Переделана основная навигация

---
+++ js/main-nav-toggle.js
@@ -0,0 +1,14 @@
+var mainNav = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+mainNav.classList.add(".main-nav-js");
+
+navToggle.addEventListener('click', function() {
+  if (mainNav.classList.contains('.main-nav--opened')) {
+    mainNav.classList.remove('.main-nav--opened');
+    mainNav.classList.add('.main-nav--closed');
+  } else {
+    mainNav.classList.remove('.main-nav--closed');
+    mainNav.classList.add('.main-nav--opened');
+  }
+})
